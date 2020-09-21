c81e753cd9e40161e68cfe4cecc22b90f2345f5b
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-09-21 13:59:03 +0200

    formuliare de connexion changé

--- connexion.php
+++ connexion.php
@@ -14,56 +14,27 @@
         <?php include 'inc/header.php';?>
     </header>
     <main id="main_connexion">
-        <h2>Formulaire de Conenxion</h2>
-        <!-- Nouveau -->
-        <div class="row">
-            <form class="col s12">
+        <h2>Formulaire de Conenxion</h2>        
+        <section class="row">
+            <form class="col s12">               
                 <div class="row">
-                    <div class="input-field col s6">
-                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                    <label for="first_name">First Name</label>
-                    </div>
-                    <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate">
-                    <label for="last_name">Last Name</label>
+                    <div class="input-field col s12">
+                        <label for="login">Login ou Email: <span class="oblig">*</span></label>
+                        <input type="text" class="validate" name="login" id="login" required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="input-field col s12">
-                    <input disabled value="I am not editable" id="disabled" type="text" class="validate">
-                    <label for="disabled">Disabled</label>
+                        <label for="password">password: <span class="oblig">*</span></label>
+                        <input type="password" class="validate" name="password" id="password" required>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                    <input id="password" type="password" class="validate">
-                    <label for="password">Password</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
-                    <label for="email">Email</label>
-                    </div>
-                </div>                                
-            </form>
-        </div>
-        <!-- Ancien -->
-            <form action="" method="POST">                
-                <section class="form-group">
-                    <label for="login">Login ou Email: <span class="oblig">*</span></label>
-                    <input type="text" class="form-control" name="login" id="login" required>
-                </section>
-                <section class="form-group">
-                    <label for="password">password: <span class="oblig">*</span></label>
-                    <input type="password" class="form-control" name="password" id="password" required>
-                </section>      
-                <section class="valid_form">
+                </div>      
+                <div class="valid_form">
                     <input type="submit" class="btn bouton" name="valid_con" value="Connexion">
-                </section>              
-                
+                </div>                                    
             </form>
-            <small>Si vous n'avez pas encore de compte => <a href="inscription.php">inscription</a></small>
+        </section>                   
+        <small>Si vous n'avez pas encore de compte => <a href="inscription.php">inscription</a></small>
     </main>
    <?php include 'inc/footer.php';?>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
