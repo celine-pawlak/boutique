c66dbdcc0634a09f21fdb0e1a95d348e65550932
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-09-21 13:48:09 +0200

    avancé switch sur materialize

--- connexion.php
+++ connexion.php
@@ -5,10 +5,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">   
     <link rel="stylesheet" href="styles/css/style.css"/>
     <title>Connexion</title>
 </head>
@@ -18,6 +15,40 @@
     </header>
     <main id="main_connexion">
         <h2>Formulaire de Conenxion</h2>
+        <!-- Nouveau -->
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s6">
+                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+                    <label for="first_name">First Name</label>
+                    </div>
+                    <div class="input-field col s6">
+                    <input id="last_name" type="text" class="validate">
+                    <label for="last_name">Last Name</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input disabled value="I am not editable" id="disabled" type="text" class="validate">
+                    <label for="disabled">Disabled</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                    </div>
+                </div>                                
+            </form>
+        </div>
+        <!-- Ancien -->
             <form action="" method="POST">                
                 <section class="form-group">
                     <label for="login">Login ou Email: <span class="oblig">*</span></label>
@@ -35,5 +66,6 @@
             <small>Si vous n'avez pas encore de compte => <a href="inscription.php">inscription</a></small>
     </main>
    <?php include 'inc/footer.php';?>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -19,93 +19,57 @@
         <div class="row">
             <form class="col s12">
                 <div class="row">
-                    <div class="input-field col s6">
-                    <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                    <label for="first_name">First Name</label>
-                    </div>
-                    <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate">
-                    <label for="last_name">Last Name</label>
-                    </div>
-                </div>               
+                    <div class="input-field col s12 m12 l6 xl6">
+                        <input type="text" class="validate" name="username" id="username" value="<?php echo $username = (isset($_POST['username']))? $_POST['username'] : '' ?>" required>
+                        <label for="username">Login <span class="oblig">*</span></label>
+                    </div>                                                  
+                    <div class="input-field col s12 m12 l6 xl6">
+                        <input type="text" class="validate" name="email" id="email" value="<?php echo $email = (isset($_POST['email']))? $_POST['email'] : '' ?>" required>
+                        <label for="email">Email <span class="oblig">*</span></label>
+                    </div>                                
+                    <div class="input-field col s12 m12 l6 xl6">
+                    <label for="password">Mot de passe: <span class="oblig">*</span></label>
+                        <input type="password" class="validate" name="password" id="password" value="<?php echo $password = (isset($_POST['password']))? $_POST['password'] : '' ?>" required>
+                        <label for="password">Mot de passe <span class="oblig">*</span></label>
+                    </div>                                
+                    <div class="input-field col s12 m12 l6 xl6">
+                        <input type="password" class="validate" name="password_confirm" id="password_confirm" value="<?php echo $password_confirm = (isset($_POST['password_confirm']))? $_POST['password_confirm'] : '' ?>" required>
+                        <label for="password_confirm">Confirmer Mot de passe <span class="oblig">*</span></label>
+                    </div>   
+                </div>        
+                <h5 class="center">Informations personnelles</h5>      
                 <div class="row">
                     <div class="input-field col s12">
-                    <input id="password" type="password" class="validate">
-                    <label for="password">Password</label>
-                    </div>
-                </div>
-                <div class="row">
+                        <label for="nom">Nom: <span class="oblig">*</span></label>
+                        <input type="text" class="validate" name="nom" id="nom" value="<?php echo $nom = (isset($_POST['nom']))? $_POST['nom'] : '' ?>" required>
+                    </div>                    
                     <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
-                    <label for="email">Email</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s12">
-                    This is an inline input field:
-                    <div class="input-field inline">
-                        <input id="email_inline" type="email" class="validate">
-                        <label for="email_inline">Email</label>
-                        <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                    </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-
-        <!-- Ancien form -->
-            <form action="" method="POST">
-                <section class="formulaire">
-                    <section id="info_log">
-                        <section class="form-group">
-                            <label for="username">Login: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="username" id="username" value="<?php echo $username = (isset($_POST['username']))? $_POST['username'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="email">Email: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="email" id="email" value="<?php echo $email = (isset($_POST['email']))? $_POST['email'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="password">Mot de passe: <span class="oblig">*</span></label>
-                            <input type="password" class="form-control" name="password" id="password" value="<?php echo $password = (isset($_POST['password']))? $_POST['password'] : '' ?>" required>
-                        </section>  
-                        <section class="form-group">
-                            <label for="password_confirm">Confirmer mot de passe: <span class="oblig">*</span></label>
-                            <input type="password" class="form-control" name="password_confirm" id="password_confirm" value="<?php echo $password_confirm = (isset($_POST['password_confirm']))? $_POST['password_confirm'] : '' ?>" required>
-                        </section>              
-                    </section>
-                    <section id="info_user">
-                        <section class="form-group">
-                            <label for="nom">Nom: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="nom" id="nom" value="<?php echo $nom = (isset($_POST['nom']))? $_POST['nom'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="prenom">Prénom: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="prenom" id="prenom" value="<?php echo $prenom = (isset($_POST['prenom']))? $_POST['prenom'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="telephone">Téléphone: </label>
-                            <input type="text" class="form-control" name="telephone" id="telephone" value="<?php echo $telephone = (isset($_POST['telephone']))? $_POST['telephone'] : '' ?>">
-                        </section>
-                        <section class="form-group">
-                            <label for="adresse">Adresse: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="adresse" id="adresse" value="<?php echo $adresse = (isset($_POST['adresse']))? $_POST['adresse'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="ville">Ville: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="ville" id="ville" value="<?php echo $ville = (isset($_POST['ville']))? $_POST['ville'] : '' ?>" required>
-                        </section>
-                        <section class="form-group">
-                            <label for="cp">Code postal: <span class="oblig">*</span></label>
-                            <input type="text" class="form-control" name="cp" id="cp" value="<?php echo $cp = (isset($_POST['cp']))? $_POST['cp'] : '' ?>" required>
-                        </section>                
-                    </section>
-                </section>    
-                <section class="valid_form">
+                        <label for="prenom">Prénom: <span class="oblig">*</span></label>
+                        <input type="text" class="form-control" name="prenom" id="prenom" value="<?php echo $prenom = (isset($_POST['prenom']))? $_POST['prenom'] : '' ?>" required>
+                    </div>                    
+                    <div class="input-field col s12">
+                        <label for="telephone">Téléphone: </label>
+                        <input type="text" class="form-control" name="telephone" id="telephone" value="<?php echo $telephone = (isset($_POST['telephone']))? $_POST['telephone'] : '' ?>">
+                    </div>                    
+                    <div class="input-field col s12">
+                        <label for="adresse">Adresse: <span class="oblig">*</span></label>
+                        <input type="text" class="form-control" name="adresse" id="adresse" value="<?php echo $adresse = (isset($_POST['adresse']))? $_POST['adresse'] : '' ?>" required>
+                    </div>                    
+                    <div class="input-field col s12">
+                        <label for="ville">Ville: <span class="oblig">*</span></label>
+                        <input type="text" class="form-control" name="ville" id="ville" value="<?php echo $ville = (isset($_POST['ville']))? $_POST['ville'] : '' ?>" required>
+                    </div>                    
+                    <div class="input-field col s12">
+                        <label for="cp">Code postal: <span class="oblig">*</span></label>
+                        <input type="text" class="form-control" name="cp" id="cp" value="<?php echo $cp = (isset($_POST['cp']))? $_POST['cp'] : '' ?>" required>
+                    </div>                    
+                </div>          
+                <div class="valid_form">
                     <input type="submit" class="btn bouton" name="valid_insc" value="Inscription">
                     <small><span class="oblig">*</span>Champs obligatoires</small>
-                </section>                          
+                </div>                
             </form>
+        </div>       
     </main>
     <?php include 'inc/footer.php';?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
