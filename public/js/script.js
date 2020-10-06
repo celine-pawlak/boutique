fbb6bf50d811b9ff44a638c5176e638dd0dbeda8
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-10-06 17:41:05 +0200

    ajout fichier json tous les produits pour barre de recherche

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,18 @@ $(document).ready(function () {
     $(".dropdown-trigger").dropdown({ hover: false, constrainWidth: false });
     $('.sidenav').sidenav();
     $('select').formSelect({constrainWidth: false });
+    
+    $('input.autocomplete').autocomplete( function(){
+        $.ajax(
+            {
+                url : 'json.php',
+                method : 'post',
+                success : (data)=>
+                    {
+                        console.log(data);
+                    }
+            }
+        );
+      });
 });
 
