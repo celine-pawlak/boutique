ffc2fe06ffcf6a1ada07494972598c4a09ed710a
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-10-06 16:48:27 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- inc/php_panier.php
+++
@@ -1,9 +0,0 @@
-<?php   
-// require_once '../class/App.php';   
-// require_once '../class/Database.php'; 
-// require_once '../class/session.php';
-    $bdd = App::getDatabase();    
-    Session::getInstance();
-   
-    
-?>
--- produits.php
+++
@@ -1,1 +0,0 @@
-<!-- envoyer dans l'url sur la page : php_add_panier.php?id_utilisateur=-&id_produit=-&quantite=- -->
