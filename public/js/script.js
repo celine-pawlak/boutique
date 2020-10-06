cd23d471c64c0ad2e11a4f7746c4297f26166700
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-10-06 18:19:02 +0200

    test tableau json

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,37 @@
 $(document).ready(function () {
     $(".dropdown-trigger").dropdown({ hover: false, constrainWidth: false });
     $('.sidenav').sidenav();
-    $('select').formSelect({constrainWidth: false });
+    $('select').formSelect({constrainWidth: false });  
     
-    $('input.autocomplete').autocomplete( function(){
-        $.ajax(
-            {
-                url : 'json.php',
-                method : 'post',
-                success : (data)=>
-                    {
-                        console.log(data);
-                    }
-            }
-        );
-      });
+    $.ajax(
+        {
+            // url : 'js/json.php',
+            // method : 'GET',
+            // success : (data)=>
+            //     {             //console.log(data);
+            //         let taille = data.length                
+            //         for(var i = 0; i<taille ; i++)
+            //             {
+            //                 console.log(data);
+            //             }
+            //     }
+        }
+    );
+    // $('input.autocomplete').autocomplete( function(){
+    //     $.ajax(
+    //         {
+    //             url : 'js/json.php',
+    //             method : 'post',
+    //             success : (data)=>
+    //                 {
+    //                     console.log(data);
+    //                     for(var i = 0; length(data); i++)
+    //                         {
+    //                             console.log(data.i);
+    //                         }
+    //                 }
+    //         }
+    //     );
+    //   });
 });
 
